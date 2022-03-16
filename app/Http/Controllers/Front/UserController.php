defd9066efc6b26f38c5af9366ee15ac688b43ae
MdDev07 <merinid1@gmail.com>
2022-03-16 14:04:36 +0100

    first commit

---
+++ app/Http/Controllers/Front/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+
+    public function Showusername(){
+        return 'test namespace succes !';
+    }
+
+    public function Showusername2(){
+        return 'test namespace succes 2 !';
+    }
+
+
+}
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/test/{id}', function ($id) {
+    return 'welcome'.$id;
+});
+
+
+/* Route::get('users/', 'App\Http\Controllers\Front\UserController@Showusername');
+
+ Route::namespace('Front')->group(function(){
+    Route::get('users/', 'UserController@Showusername');
+});  */
+
+Route::group(['namespace' => 'App\Http\Controllers\Front'], function()
+{
+    Route::get('users/', 'UserController@Showusername');
+    Route::get('users2/', 'UserController@Showusername2');
+});
+
